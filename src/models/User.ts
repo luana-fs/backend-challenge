9dbcbd3d4bc100b25769666c4e68e106d4feed8d
Luana Farias <lua.f@hotmail.com>
2022-12-30 00:16:00 -0300

    model and repository dir implemented

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: number;
+
+  constructor() {}
+}
+
+export { User };
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,9 @@
+import { User } from "../models/User";
+
+class UserRepository {
+  private user: User[];
+
+  constructor() {}
+}
+
+export { UserRepository };
