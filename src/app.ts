600f83685900f18b93069c0dd508c9ebe870bf2e
Luana Farias <lua.f@hotmail.com>
2023-01-03 18:27:42 -0300

    swagger implementation in progress

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 app.use(cors());
 
 //esse middleware transforma os bodys recebidos em json para objeto
 app.use(express.json());
+
+//cria o servidor e um link para o swagger, além do arquivo json de config
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 //importa as rotas
 app.use(router);
