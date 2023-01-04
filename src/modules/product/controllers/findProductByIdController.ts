ed87adb655832e2b2584230e548d7f7297780cbb
Luana Farias <lua.f@hotmail.com>
2023-01-04 17:24:28 -0300

    products error treatment

--- src/modules/product/controllers/findProductByIdController.ts
+++ src/modules/product/controllers/findProductByIdController.ts
@@ -2,9 +2,13 @@ import { productRepository } from "../services";
 import { Request, Response } from "express";
 
 const findProductByIdController = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const product = await productRepository.findById(Number(id));
-  res.status(200).send(product);
+  try {
+    const { id } = req.params;
+    const product = await productRepository.findById(Number(id));
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 };
 
 export { findProductByIdController };
--- src/modules/product/controllers/getAllProductsController.ts.ts
+++ src/modules/product/controllers/getAllProductsController.ts.ts
@@ -2,13 +2,17 @@ import { productRepository } from "../services";
 import { Request, Response } from "express";
 
 const getAllProductsController = async (req: Request, res: Response) => {
-  const { name } = req.query;
-  if (name) {
-    const product = await productRepository.findByName(name as string);
-    return res.status(200).send(product);
+  try {
+    const { name } = req.query;
+    if (name) {
+      const product = await productRepository.findByName(name as string);
+      return res.status(200).send(product);
+    }
+    const products = await productRepository.list();
+    return res.status(200).send(products);
+  } catch (error) {
+    return res.status(400).send(error.message);
   }
-  const products = await productRepository.list();
-  return res.status(200).send(products);
 };
 
 export { getAllProductsController };
