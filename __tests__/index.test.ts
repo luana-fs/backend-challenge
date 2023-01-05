c21669a8fabf6cd8d2b98923249caff80220decc
Luana Farias <lua.f@hotmail.com>
2023-01-05 13:45:06 -0300

    tests implementation in progress

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -6,16 +6,40 @@
 // });
 
 const request = require("supertest");
-import app from "../src/app";
+import { app } from "../src/app";
 
 test("Should signup a new user", async () => {
   const response = await request(app)
     .post("/users")
     .send({
-      name: "paulo",
-      email: "paulo10@test.com",
+      name: "hello2",
+      email: "hello2@test.com",
       password: "1234567890",
+      confirmPassword: "1234567890",
       role: 1,
     })
     .expect(201);
+});
+
+test("Should not create duplicate users", async () => {
+  const response = await request(app)
+    .post("/users")
+    .send({
+      name: "bea",
+      email: "bea@test.com",
+      password: "1234567890",
+      confirmPassword: "1234567890",
+      role: 1,
+    })
+
+    .post("/users")
+    .send({
+      name: "bea",
+      email: "bea@test.com",
+      password: "1234567890",
+      confirmPassword: "1234567890",
+      role: 1,
+    })
+    .expect(400);
+  expect(response.body.error).toBeTruthy();
 });
